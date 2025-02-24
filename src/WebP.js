67a70f3cc38ab9fc3f5378dd5a48e7dd1f711254
caed0 <62652990+caed0@users.noreply.github.com>
2025-02-24 20:03:23 +0100

    change binary import

--- src/WebP.js
+++ src/WebP.js
@@ -1,27 +1,45 @@
-const anim_diff = require('modules/anim_diff');
-const anim_dump = require('modules/anim_dump');
-const cwebp = require('modules/cwebp');
-const dwebp = require('modules/dwebp');
-const get_disto = require('modules/get_disto');
-const gif2webp = require('modules/gif2webp');
-const img2webp = require('modules/img2webp');
-const vwebp = require('modules/vwebp');
-const webp_quality = require('modules/webp_quality');
-const webpinfo = require('modules/webpinfo');
-const webpmux = require('modules/webpmux');
+const { execSync } = require('child_process');
+
+let libwebp;
+try {
+  libwebp = require('libwebp-static');
+} catch (e) {}
+
 
 class WebP {
   constructor() {
-    this.anim_diff = anim_diff;
-    this.anim_dump = anim_dump;
-    this.cwebp = cwebp;
-    this.dwebp = dwebp;
-    this.get_disto = get_disto;
-    this.gif2webp = gif2webp;
-    this.img2webp = img2webp;
-    this.vwebp = vwebp;
-    this.webp_quality = webp_quality;
-    this.webpinfo = webpinfo;
-    this.webpmux = webpmux;
+    if (execSync('command -v dwebep').toString().trim()) {
+      this.binaries = {
+        anim_diff: 'anim_diff',
+        anim_dump: 'anim_dump',
+        cwebp: 'cwebp',
+        dwebp: 'dwebp',
+        get_disto: 'get_disto',
+        gif2webp: 'gif2webp',
+        img2webp: 'img2webp',
+        vwebp: 'vwebp',
+        webp_quality: 'webp_quality',
+        webpinfo: 'webpinfo',
+        webpmux: 'webpmux'
+      };
+    } else if (libwebp) {
+      this.binaries = libwebp;
+    } else {
+        throw new Error('WebP binaries not found');
+    }
+
+    this.cwebp = require('modules/cwebp')(this.binaries.cwebp);
+    this.dwebp = require('modules/dwebp')(this.binaries.dwebp);
+    this.webpinfo = require('modules/webpinfo')(this.binaries.webpinfo);
+    this.webpmux = require('modules/webpmux')(this.binaries.webpmux);
+    this.anim_dump = require('modules/anim_dump')(this.binaries.anim_dump);
+    //this.anim_diff = require('modules/anim_diff')(this.binaries.anim_diff);
+    //this.get_disto = require('modules/get_disto')(this.binaries.get_disto);
+    //this.gif2webp = require('modules/gif2webp')(this.binaries.gif2webp);
+    //this.img2webp = require('modules/img2webp')(this.binaries.img2webp);
+    //this.vwebp = require('modules/vwebp')(this.binaries.vwebp);
+    //this.webp_quality = require('modules/webp_quality')(this.binaries.webp_quality);
   }
 }
+
+module.exports = WebP;
--- src/modules/dwebp.js
+++ src/modules/dwebp.js
@@ -1,9 +1,74 @@
 const { execSync } = require('child_process');
 const path = require('path');
+const { validateOptions } = require('../utils/validate');
 
-module.exports = (inputFile, options = {}, outputFile) => {
-    let command = `dwebp ${inputFile}`;
-    const validExtensions = ['pam', 'ppm', 'bmp', 'tif', 'tiff', 'pgm', 'yuv', 'png'];
+const VALID_OPTIONS = {
+    format: 'string',
+    nofancy: 'boolean',
+    nofilter: 'boolean',
+    nodither: 'boolean',
+    dither: 'number',
+    alpha_dither: 'boolean',
+    mt: 'boolean',
+    crop: 'object',
+    resize: 'object',
+    flip: 'boolean',
+    alpha: 'boolean',
+    noasm: 'boolean'
+};
+
+const VALID_FORMATS = ['pam', 'ppm', 'bmp', 'tiff', 'pgm', 'yuv', 'png'];
+
+const VALID_CROP = {
+    x: 'number',
+    y: 'number',
+    w: 'number',
+    h: 'number'
+}
+
+const VALID_RESIZE = {
+    w: 'number',
+    h: 'number'
+}
+
+module.exports = (dwebp) => (inputFile, outputFile = undefined, options = {}) => {
+    const { format, nofancy, nofilter, nodither, dither, alpha_dither, mt, crop, resize, flip, alpha, noasm } = options;
+
+    let invalid = validateOptions(options, VALID_OPTIONS);
+    if (invalid)
+        throw new Error(invalid);
+
+    const ext = path.extname(outputFile).slice(1);
+    if (format && !VALID_FORMATS.includes(format))
+        throw new Error(`Invalid output file format: ${ext}`);
+
+    if (dither && (dither > 100 || dither < 0))
+        throw new Error(`Dithering strength out of range: expected 0 ... 100, got ${dither}`);
+
+    invalid = validateOptions(crop, VALID_CROP);
+    if (invalid) throw new Error();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    const args = [inputFile, '-quite'];
 
     if (outputFile) {
         const ext = path.extname(outputFile).slice(1);
@@ -13,54 +78,47 @@ module.exports = (inputFile, options = {}, outputFile) => {
 
         switch (ext) {
             case 'pam':
-                command += ' -pam';
+                args.push('-pam');
                 break;
             case 'ppm':
-                command += ' -ppm';
+                args.push('-ppm');
                 break;
             case 'bmp':
-                command += ' -bmp';
+                args.push('-bmp');
                 break;
             case 'tif':
-                command += ' -tiff';
-                break;
             case 'tiff':
-                command += ' -tiff';
+                args.push('-tiff');
                 break;
             case 'pgm':
-                command += ' -pgm';
+                args.push('-pgm');
                 break;
             case 'yuv':
-                command += ' -yuv';
+                args.push('-yuv');
                 break;
             default:
                 break;
         }
-
-        command += ` -o ${outputFile}`;
     } else {
         throw new Error('Output file must be specified');
     }
 
-    if (options.nofancy) command += ' -nofancy';
-    if (options.nofilter) command += ' -nofilter';
+    if (options.nofancy) args.push('-nofancy');
+    if (options.nofilter) args.push('-nofilter');
     if (options.nodither) {
-        command += ' -nodither';
+        args.push('-nodither');
         options.dither = undefined;
+        options.alpha_dither = false;
     }
-    if (options.dither !== undefined) command += ` -dither ${options.dither}`;
-    if (options.alpha_dither) command += ' -alpha_dither';
-    if (options.mt) command += ' -mt';
-    if (options.crop) command += ` -crop ${options.crop.x} ${options.crop.y} ${options.crop.w} ${options.crop.h}`;
-    if (options.resize) command += ` -resize ${options.resize.w} ${options.resize.h}`;
-    if (options.flip) command += ' -flip';
-    if (options.alpha) command += ' -alpha';
-    command += ' -quiet';
-    if (options.noasm) command += ' -noasm';
+    if (options.dither !== undefined) args.push(`-dither`, options.dither);
+    if (options.alpha_dither) args.push('-alpha_dither');
+    if (options.mt) args.push('-mt');
+    if (options.crop) args.push(`-crop`, options.crop.x, options.crop.y, options.crop.w, options.crop.h);
+    if (options.resize) args.push(`-resize`, options.resize.w, options.resize.h);
+    if (options.flip) args.push('-flip');
+    if (options.alpha) args.push('-alpha');
+    if (options.noasm) args.push('-noasm');
 
-    try {
-        execSync(command, { stdio: 'inherit' });
-    } catch (error) {
-        console.error('Error executing dwebp command:', error);
-    }
+    args.push('-o', outputFile);
+    execSync(dwebp, args, (error) => { if (error) console.error('Error executing dwebp command:', error); });
 }
